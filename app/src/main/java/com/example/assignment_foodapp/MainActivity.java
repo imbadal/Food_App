0ebfd43bf9250a2c4b2960d0991657844718cef4
Badal Kumar <s.k.badal@outlook.com>
2019-03-12 10:39:46 +0530

    auto slide

--- app/src/main/java/com/example/assignment_foodapp/MainActivity.java
+++ app/src/main/java/com/example/assignment_foodapp/MainActivity.java
@@ -3,41 +3,120 @@ package com.example.assignment_foodapp;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     ViewPager viewPager;
     FragmentPagerAdapter adapter;
 
-    ActionBar actionBar;
 
+    int currentPage = 0;
+    Timer timer;
+    final long DELAY_MS = 1000;//delay in milliseconds before task is to be executed
+    final long PERIOD_MS = 5000; // time in milliseconds between successive task executions.
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
         setContentView(R.layout.activity_main);
 
-        actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(null);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setBackgroundDrawable(new ColorDrawable(0x70000000));
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window w = getWindow();
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
+        getWindow().setStatusBarColor(0x70000000);
 
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00000000")));
+        getWindow().setStatusBarColor(0x66000000);
+
 
         viewPager = findViewById(R.id.vpPager);
         adapter = new MyPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
+
+//        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int i, float v, int i1) {
+//
+//            }
+//
+//            @Override
+//            public void onPageSelected(int i) {
+//
+//                if (i == 0) {
+//
+//                    ProgressBar progressBar = findViewById(R.id.progressbar1);
+//                    progressBar.setProgress(0);
+//                    Log.d("test_", "onPageSelected:1 ");
+//
+//                } else if (i == 1) {
+//
+//                    Log.d("test_", "onPageSelected:2 ");
+//
+//
+//                } else if (i == 2) {
+//
+//
+//                } else {
+//
+//
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int i) {
+//
+//            }
+//        });
+
+        final Handler handler = new Handler();
+
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (currentPage == 3) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(runnable);
+            }
+        }, DELAY_MS, PERIOD_MS);
+
 
     }
 }
--- app/src/main/java/com/example/assignment_foodapp/ThirdFragment.java
+++ app/src/main/java/com/example/assignment_foodapp/ThirdFragment.java
@@ -1,14 +1,21 @@
 package com.example.assignment_foodapp;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 public class ThirdFragment extends Fragment {
+
+    ProgressBar progressBar;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,7 +27,38 @@ public class ThirdFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_third,container,false);
+
+        progressBar = view.findViewById(R.id.progressbar3);
+        progressBar.setProgress(0);
+
+        ValueAnimator animator = ValueAnimator.ofInt(0, progressBar.getMax());
+        animator.setDuration(5000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                progressBar.setProgress((Integer) animation.getAnimatedValue());
+            }
+        });
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                // start your activity here
+            }
+        });
+        animator.start();
+
         return view;
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Log.d("test_", "onStart: ");
+        progressBar.setProgress(0);
+
+    }
+
 }
